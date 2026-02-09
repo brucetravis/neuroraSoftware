3006df4f30d7c53e4fd44a373e8dc23cdc718403
Bruce Ambundo <bruceambundo@gmail.com>
2026-02-09 17:58:54 +0300

    Loading page created

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Home from './pages/home/Home';
 import "react-toastify/dist/ReactToastify.css";
@@ -6,12 +7,30 @@ import { Route, Routes } from 'react-router-dom';
 import { aiPlans, swPlans } from './data/Pricing';
 import { usePricing } from './contexts/PricingProvider';
 import ChatWidget from './components/chatwidget/ChatWidget';
+import LoadingPage from './components/loadingpage/LoadingPage';
 import CustomQuoteModal from './components/modals/custom/CustomQuoteModal';
 import StandardPricingModal from './components/modals/standard/StandardPricingModal';
 
 function App() {
 
   const { openStandardModal, openQuotationModal } = usePricing();
+
+  // states to handle the loading page
+  const [ showLoading, setShowLoading ] = useState(true)
+
+  // useEffect to handle the animations
+  useEffect(() => {
+    // simulate the loading duration, later we can tie this to animation finish
+    const timer = setTimeout(() => setShowLoading(false), 30000)
+
+    // clear the timer
+    return () => clearTimeout(timer)
+
+  }, []) // empty dependency array
+  
+
+  // if loading is true, show the loading page first
+  //if (showLoading) return <LoadingPage onFinish={() => setShowLoading(false)} />
 
   return (
     <>
@@ -22,6 +41,9 @@ function App() {
         
         
         <ChatWidget />
+
+        {/* Overlay loading page */}
+        {showLoading && <LoadingPage onFinish={() => setShowLoading(false)} />}
       </div>
 
       {openStandardModal && <StandardPricingModal aiPlans={aiPlans} swPlans={swPlans} />}

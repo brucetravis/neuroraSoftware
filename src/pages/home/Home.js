839b901112fe4f9849d8f589a8454eda2c23c0c0
Bruce Ambundo <bruceambundo@gmail.com>
2026-02-09 15:18:02 +0300

    Why Us section created

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -17,9 +17,9 @@ export default function Home() {
 
   return (
     <div>
-        <section ref={whyUs}><WhyUs /></section>
-        <section ref={pricing}><Pricing /></section>
-        <section ref={contact}><Contact /></section>
+      <section ref={whyUs}><WhyUs /></section>
+      <section ref={pricing}><Pricing /></section>
+      <section ref={contact}><Contact /></section>
     </div>
   )
 }

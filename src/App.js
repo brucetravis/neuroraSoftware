152a77c2bbb3ba7264ea8c64e71b6835e920d481
Bruce Ambundo <bruceambundo@gmail.com>
2026-02-10 13:37:38 +0300

    ChatWidget restored

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { usePricing } from './contexts/PricingProvider';
 import LoadingPage from './components/loadingpage/LoadingPage';
 import CustomQuoteModal from './components/modals/custom/CustomQuoteModal';
 import StandardPricingModal from './components/modals/standard/StandardPricingModal';
+import ChatWidget from './components/chatwidget/ChatWidget';
 
 function App() {
 
@@ -37,6 +38,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
+
+        <ChatWidget />
         
         {/* Overlay loading page */}
         {showLoading && <LoadingPage onFinish={() => setShowLoading(false)} />}
